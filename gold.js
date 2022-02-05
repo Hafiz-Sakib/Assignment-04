4e088b9500907ea8db5e6f4be0818869c91935ff
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-02-05 09:17:19 +0600

    first commit

---
+++ gold.js
@@ -0,0 +1,13 @@
+/* This is the Function For Converting Gold Weight From Ana To Vori */
+
+function AnaToVori(ana) {
+  if (typeof ana != "number") {
+    console.log("Please Enter a Number");
+  }
+  let vori = ana / 16;
+  return vori;
+}
+
+let gold = AnaToVori(16);
+console.log(gold);
+/* Ana To Vori Function Ends */
